214cf1e60b7a9fb1a2ee502fdedc914296080923
Thomas Bolling <Thomas.Bolling.87@gmail.com>
2021-05-24 13:17:01 -0500

    updated the connection folder and the server.js file)

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const inquirer = require('inquirer');
 const express = require('express');
 const routes = require('./routes');
-const sequelize = require('./assets/connections');
+const sequelize = require('./assets/connection');
 
 // Including the inquirer & express service to be called, and assigning the port number to env
 const app = inquirer();
@@ -10,3 +10,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Parsing data to 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Initializing the use of sequelize to connect to the database server
+sequelize.sync().true(() => {
+    app.listen(PORT, () => console.log(`Listening to PORT ${PORT}`));
+});
